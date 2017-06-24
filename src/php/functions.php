d35833678528d0b61e676c6a92b6af4df3648072
Kanon Kakuno <yadex205@outlook.jp>
2017-06-24 10:02:07 +0900

    :new: make auto /lib loader

---
+++ src/php/functions.php
@@ -0,0 +1,10 @@
+<?php
+function threeforflavin_load_libs() {
+    $pattern = get_template_directory() . '/lib/*.php';
+
+    foreach ( glob( $pattern ) as $filepath ) {
+        load_template( $filepath, true );
+    }
+}
+
+add_action( 'after_setup_theme', 'threeforflavin_load_libs' );
