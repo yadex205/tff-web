7a6e16bbb8d9c832f2286cc3d8878f101a5e75fb
Kanon Kakuno <yadex205@outlook.jp>
2017-07-20 10:46:17 +0900

    :+1: add topics rows and insert twitter widget

--- src/php/home.php
+++ src/php/home.php
@@ -8,4 +8,29 @@ $welcome_image_uri = get_template_directory_uri() . '/image/home-welcome.jpg';
   <img class="welcome-image" alt="welcome-image" src="<?php echo $welcome_image_uri; ?>"/>
 </div>
 
+<div class="home-topics">
+  <div class="home-topics-item">
+    <header class="home-topics-item-header">
+      Live
+    </header>
+  </div>
+  <div class="home-topics-item">
+    <header class="home-topics-item-header">
+      News
+    </header>
+  </div>
+  <div class="home-topics-item">
+    <header class="home-topics-item-header">
+      Tweet
+    </header>
+    <a class="twitter-timeline"
+       data-theme="light"
+       data-tweet-limit="4"
+       data-link-color="#dcdc42"
+       data-chrome="transparent nofooter noheader noscrollbar"
+       href="https://twitter.com/ThreeforFlavin">Tweets by ThreeforFlavin</a>
+  </div>
+</div>
+
+
 <?php get_footer(); ?>
