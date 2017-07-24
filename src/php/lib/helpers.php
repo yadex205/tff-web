d6234dc35f9b0595099a48095546955fc45a503a
Kanon Kakuno <yadex205@outlook.jp>
2017-07-24 11:06:44 +0900

    :+1: implement show future post and get live posts

--- src/php/lib/helpers.php
+++ src/php/lib/helpers.php
@@ -6,3 +6,12 @@ function get_vendor_uri() {
 function get_header_logo_uri( $zoom = '' ) {
     return get_template_directory_uri() . '/image/header-logo' . $zoom . '.png';
 }
+
+function get_scheduled_live_posts( $tag = '', $limit = -1 ) {
+    $posts = get_posts( array( 'category_name'  => 'live',
+                               'tag'            => $tag,
+                               'posts_per_page' => $limit,
+                               'post_status'    => 'publish,future',
+                               'date_query'     => array( 'inclusive' => true, 'after' => date_i18n( 'Y/n/j' ) ) ) );
+    return array_reverse( $posts );
+}
